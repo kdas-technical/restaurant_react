8c60744375849d2d71869a3f7cbde5e947422aca
Kunal Kumae Das <das.kunal02@gmail.com>
2026-06-16 21:32:45 +0530

    commit project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      
+      <h1 className='text-center text-6xl bg-emerald-600 text-orange-600 py-4'>Vite + React</h1>
+  
+    </>
+  )
+}
+
+export default App
